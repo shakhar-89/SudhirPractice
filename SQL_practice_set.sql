9b8ac44e38485b9eb9e5f34d326fa7f3b56532f5
sudhir Kumar <shakhar.89@gmail.com>
2022-04-19 00:57:11 +0530

    added 44

--- SQL_practice_set.sql
+++ SQL_practice_set.sql
@@ -2128,5 +2128,131 @@ Output:
 Follow up: If it is unknown which continent has the most students, could you write a query to generate the student report?
 
 --solution :
+Select
+ distinct 
+ min(case when continent = 'America' then name else null end) as "America",
+ min(case when continent = 'Asia' then name else null end) as "Asia",
+ min(case when continent = 'Europe' then name else null end) as "Europe"
+from ( 
+      select dense_rank() over(partition by continent order by name) rnk,
+             name,
+             continent
+       from student 
+    ) T
+group by rnk
+order by 1,2,3
 
+
+44. https://leetcode.com/problems/leetcodify-friends-recommendations/
+
+Leetcodify Friends Recommendations
+
+SQL Schema
+Table: Listens
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| user_id     | int     |
+| song_id     | int     |
+| day         | date    |
++-------------+---------+
+There is no primary key for this table. It may contain duplicates.
+Each row of this table indicates that the user user_id listened to the song song_id on the day day.
+ 
+
+Table: Friendship
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| user1_id      | int     |
+| user2_id      | int     |
++---------------+---------+
+(user1_id, user2_id) is the primary key for this table.
+Each row of this table indicates that the users user1_id and user2_id are friends.
+Note that user1_id < user2_id.
+ 
+
+Write an SQL query to recommend friends to Leetcodify users. We recommend user x to user y if:
+
+Users x and y are not friends, and
+Users x and y listened to the same three or more different songs on the same day.
+Note that friend recommendations are unidirectional, meaning if user x and user y should be recommended to each other, the result table should have both user x recommended to user y and user y recommended to user x. Also, note that the result table should not contain duplicates (i.e., user y should not be recommended to user x multiple times.).
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Listens table:
++---------+---------+------------+
+| user_id | song_id | day        |
++---------+---------+------------+
+| 1       | 10      | 2021-03-15 |
+| 1       | 11      | 2021-03-15 |
+| 1       | 12      | 2021-03-15 |
+| 2       | 10      | 2021-03-15 |
+| 2       | 11      | 2021-03-15 |
+| 2       | 12      | 2021-03-15 |
+| 3       | 10      | 2021-03-15 |
+| 3       | 11      | 2021-03-15 |
+| 3       | 12      | 2021-03-15 |
+| 4       | 10      | 2021-03-15 |
+| 4       | 11      | 2021-03-15 |
+| 4       | 13      | 2021-03-15 |
+| 5       | 10      | 2021-03-16 |
+| 5       | 11      | 2021-03-16 |
+| 5       | 12      | 2021-03-16 |
++---------+---------+------------+
+Friendship table:
++----------+----------+
+| user1_id | user2_id |
++----------+----------+
+| 1        | 2        |
++----------+----------+
+Output: 
++---------+----------------+
+| user_id | recommended_id |
++---------+----------------+
+| 1       | 3              |
+| 2       | 3              |
+| 3       | 1              |
+| 3       | 2              |
++---------+----------------+
+Explanation: 
+Users 1 and 2 listened to songs 10, 11, and 12 on the same day, but they are already friends.
+Users 1 and 3 listened to songs 10, 11, and 12 on the same day. Since they are not friends, we recommend them to each other.
+Users 1 and 4 did not listen to the same three songs.
+Users 1 and 5 listened to songs 10, 11, and 12, but on different days.
+
+Similarly, we can see that users 2 and 3 listened to songs 10, 11, and 12 on the same day and are not friends, so we recommend them to each other.
+
+--solution:
+--deduplicate listens into temp1
+--self join temp1 on (day and song) and filter pair of different ids
+-- number of distinct songs listened by above pair on same day should be >=3
+-- filter out pair that are already friends.
+
+with temp1 as (
+    select distinct user_id user_id,song_id song_id ,to_char(day,'yyyy-mm-dd') day 
+    from listens),
+temp2 as (
+select f.user_id "user_id",s.user_id "recommended_id"
+ from temp1 f 
+  inner join temp1 s on f.song_id = s.song_id and f.day = s.day
+ where f.user_id <> s.user_id
+group by f.user_id,s.user_id,f.day
+having count( distinct f.song_id ) >= 3
+)
+select distinct "user_id","recommended_id" 
+ from temp2 
+-- filter out pair that are already friends.
+left join friendship f on ( ("user_id" = user1_id and "recommended_id" = user2_id)
+                          or ("user_id" = user2_id and "recommended_id" = user1_id))                          
+where f.user1_id is null or f.user2_id is null
 
