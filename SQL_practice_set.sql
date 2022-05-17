05c9655e7b2887f2fe915191c72eb6e754e87652
sudhir Kumar <shakhar.89@gmail.com>
2022-05-17 16:49:08 +0530

    Update SQL_practice_set.sql

--- SQL_practice_set.sql
+++ SQL_practice_set.sql
@@ -3913,3 +3913,479 @@ temp2 as ( select
     inner join student s 
   on t.student_id = s.student_id
  order by 1
+
+ 66. https://leetcode.com/problems/sales-by-day-of-the-week/ 
+
+Sales by Day of the Week
+
+Table: Orders
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| order_id      | int     |
+| customer_id   | int     |
+| order_date    | date    | 
+| item_id       | varchar |
+| quantity      | int     |
++---------------+---------+
+(ordered_id, item_id) is the primary key for this table.
+This table contains information on the orders placed.
+order_date is the date item_id was ordered by the customer with id customer_id.
+ 
+
+Table: Items
+
++---------------------+---------+
+| Column Name         | Type    |
++---------------------+---------+
+| item_id             | varchar |
+| item_name           | varchar |
+| item_category       | varchar |
++---------------------+---------+
+item_id is the primary key for this table.
+item_name is the name of the item.
+item_category is the category of the item.
+ 
+
+You are the business owner and would like to obtain a sales report for category items and the day of the week.
+
+Write an SQL query to report how many units in each category have been ordered on each day of the week.
+
+Return the result table ordered by category.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Orders table:
++------------+--------------+-------------+--------------+-------------+
+| order_id   | customer_id  | order_date  | item_id      | quantity    |
++------------+--------------+-------------+--------------+-------------+
+| 1          | 1            | 2020-06-01  | 1            | 10          |
+| 2          | 1            | 2020-06-08  | 2            | 10          |
+| 3          | 2            | 2020-06-02  | 1            | 5           |
+| 4          | 3            | 2020-06-03  | 3            | 5           |
+| 5          | 4            | 2020-06-04  | 4            | 1           |
+| 6          | 4            | 2020-06-05  | 5            | 5           |
+| 7          | 5            | 2020-06-05  | 1            | 10          |
+| 8          | 5            | 2020-06-14  | 4            | 5           |
+| 9          | 5            | 2020-06-21  | 3            | 5           |
++------------+--------------+-------------+--------------+-------------+
+Items table:
++------------+----------------+---------------+
+| item_id    | item_name      | item_category |
++------------+----------------+---------------+
+| 1          | LC Alg. Book   | Book          |
+| 2          | LC DB. Book    | Book          |
+| 3          | LC SmarthPhone | Phone         |
+| 4          | LC Phone 2020  | Phone         |
+| 5          | LC SmartGlass  | Glasses       |
+| 6          | LC T-Shirt XL  | T-Shirt       |
++------------+----------------+---------------+
+Output: 
++------------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+
+| Category   | Monday    | Tuesday   | Wednesday | Thursday  | Friday    | Saturday  | Sunday    |
++------------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+
+| Book       | 20        | 5         | 0         | 0         | 10        | 0         | 0         |
+| Glasses    | 0         | 0         | 0         | 0         | 5         | 0         | 0         |
+| Phone      | 0         | 0         | 5         | 1         | 0         | 0         | 10        |
+| T-Shirt    | 0         | 0         | 0         | 0         | 0         | 0         | 0         |
++------------+-----------+-----------+-----------+-----------+-----------+-----------+-----------+
+Explanation: 
+On Monday (2020-06-01, 2020-06-08) were sold a total of 20 units (10 + 10) in the category Book (ids: 1, 2).
+On Tuesday (2020-06-02) were sold a total of 5 units in the category Book (ids: 1, 2).
+On Wednesday (2020-06-03) were sold a total of 5 units in the category Phone (ids: 3, 4).
+On Thursday (2020-06-04) were sold a total of 1 unit in the category Phone (ids: 3, 4).
+On Friday (2020-06-05) were sold 10 units in the category Book (ids: 1, 2) and 5 units in Glasses (ids: 5).
+On Saturday there are no items sold.
+On Sunday (2020-06-14, 2020-06-21) were sold a total of 10 units (5 +5) in the category Phone (ids: 3, 4).
+There are no sales of T-shirts.
+
+--solution
+/* Write your PL/SQL query statement below */
+
+with temp as (
+select o.*,trim(to_char(order_date,'DAY'))  day
+ from Orders o
+) select 
+    i.item_category category,
+    SUM(CASE WHEN DAY = 'MONDAY' THEN QUANTITY ELSE 0 END) AS "MONDAY",
+    SUM(CASE WHEN DAY = 'TUESDAY' THEN QUANTITY ELSE 0 END) AS "TUESDAY",
+    SUM(CASE WHEN DAY = 'WEDNESDAY' THEN QUANTITY ELSE 0 END) AS "WEDNESDAY",
+    SUM(CASE WHEN DAY = 'THURSDAY' THEN QUANTITY ELSE 0 END) AS "THURSDAY",
+    SUM(CASE WHEN DAY = 'FRIDAY' THEN QUANTITY ELSE 0 END) AS "FRIDAY",
+    SUM(CASE WHEN DAY = 'SATURDAY' THEN QUANTITY ELSE 0 END) AS "SATURDAY",
+    SUM(CASE WHEN DAY = 'SUNDAY' THEN QUANTITY ELSE 0 END) AS "SUNDAY"
+ from 
+  Items i left join temp t on i.item_id = t.item_id  
+ group by i.item_category
+ order by i.item_category
+
+67. https://leetcode.com/problems/hopper-company-queries-i/ 
+
+Hopper Company Queries I
+
+SQL Schema
+Table: Drivers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| driver_id   | int     |
+| join_date   | date    |
++-------------+---------+
+driver_id is the primary key for this table.
+Each row of this table contains the drivers ID and the date they joined the Hopper company.
+ 
+
+Table: Rides
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| ride_id      | int     |
+| user_id      | int     |
+| requested_at | date    |
++--------------+---------+
+ride_id is the primary key for this table.
+Each row of this table contains the ID of a ride, the users ID that requested it, and the day they requested it.
+There may be some ride requests in this table that were not accepted.
+ 
+
+Table: AcceptedRides
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| ride_id       | int     |
+| driver_id     | int     |
+| ride_distance | int     |
+| ride_duration | int     |
++---------------+---------+
+ride_id is the primary key for this table.
+Each row of this table contains some information about an accepted ride.
+It is guaranteed that each accepted ride exists in the Rides table.
+ 
+
+Write an SQL query to report the following statistics for each month of 2020:
+
+The number of drivers currently with the Hopper company by the end of the month (active_drivers).
+The number of accepted rides in that month (accepted_rides).
+Return the result table ordered by month in ascending order, where month is the months number (January is 1, February is 2, etc.).
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Drivers table:
++-----------+------------+
+| driver_id | join_date  |
++-----------+------------+
+| 10        | 2019-12-10 |
+| 8         | 2020-1-13  |
+| 5         | 2020-2-16  |
+| 7         | 2020-3-8   |
+| 4         | 2020-5-17  |
+| 1         | 2020-10-24 |
+| 6         | 2021-1-5   |
++-----------+------------+
+Rides table:
++---------+---------+--------------+
+| ride_id | user_id | requested_at |
++---------+---------+--------------+
+| 6       | 75      | 2019-12-9    |
+| 1       | 54      | 2020-2-9     |
+| 10      | 63      | 2020-3-4     |
+| 19      | 39      | 2020-4-6     |
+| 3       | 41      | 2020-6-3     |
+| 13      | 52      | 2020-6-22    |
+| 7       | 69      | 2020-7-16    |
+| 17      | 70      | 2020-8-25    |
+| 20      | 81      | 2020-11-2    |
+| 5       | 57      | 2020-11-9    |
+| 2       | 42      | 2020-12-9    |
+| 11      | 68      | 2021-1-11    |
+| 15      | 32      | 2021-1-17    |
+| 12      | 11      | 2021-1-19    |
+| 14      | 18      | 2021-1-27    |
++---------+---------+--------------+
+AcceptedRides table:
++---------+-----------+---------------+---------------+
+| ride_id | driver_id | ride_distance | ride_duration |
++---------+-----------+---------------+---------------+
+| 10      | 10        | 63            | 38            |
+| 13      | 10        | 73            | 96            |
+| 7       | 8         | 100           | 28            |
+| 17      | 7         | 119           | 68            |
+| 20      | 1         | 121           | 92            |
+| 5       | 7         | 42            | 101           |
+| 2       | 4         | 6             | 38            |
+| 11      | 8         | 37            | 43            |
+| 15      | 8         | 108           | 82            |
+| 12      | 8         | 38            | 34            |
+| 14      | 1         | 90            | 74            |
++---------+-----------+---------------+---------------+
+Output: 
++-------+----------------+----------------+
+| month | active_drivers | accepted_rides |
++-------+----------------+----------------+
+| 1     | 2              | 0              |
+| 2     | 3              | 0              |
+| 3     | 4              | 1              |
+| 4     | 4              | 0              |
+| 5     | 5              | 0              |
+| 6     | 5              | 1              |
+| 7     | 5              | 1              |
+| 8     | 5              | 1              |
+| 9     | 5              | 0              |
+| 10    | 6              | 0              |
+| 11    | 6              | 2              |
+| 12    | 6              | 1              |
++-------+----------------+----------------+
+Explanation: 
+By the end of January --> two active drivers (10, 8) and no accepted rides.
+By the end of February --> three active drivers (10, 8, 5) and no accepted rides.
+By the end of March --> four active drivers (10, 8, 5, 7) and one accepted ride (10).
+By the end of April --> four active drivers (10, 8, 5, 7) and no accepted rides.
+By the end of May --> five active drivers (10, 8, 5, 7, 4) and no accepted rides.
+By the end of June --> five active drivers (10, 8, 5, 7, 4) and one accepted ride (13).
+By the end of July --> five active drivers (10, 8, 5, 7, 4) and one accepted ride (7).
+By the end of August --> five active drivers (10, 8, 5, 7, 4) and one accepted ride (17).
+By the end of September --> five active drivers (10, 8, 5, 7, 4) and no accepted rides.
+By the end of October --> six active drivers (10, 8, 5, 7, 4, 1) and no accepted rides.
+By the end of November --> six active drivers (10, 8, 5, 7, 4, 1) and two accepted rides (20, 5).
+By the end of December --> six active drivers (10, 8, 5, 7, 4, 1) and one accepted ride (2).
+
+--solution1
+/* Write your PL/SQL query statement below */
+with cte(yr,month) as (
+    select 2020,1 from dual 
+     union all
+    select 2020,month + 1 from cte 
+    where month+1 <= 12
+) ,
+cte2 as (
+select 
+ coalesce(yr,to_number(to_char(join_date,'yyyy'))) yr,
+ coalesce(month,to_number(to_char(join_date,'mm'))) month,
+ driver_id
+from cte c 
+ full outer join Drivers d 
+  on to_number(to_char(join_date,'mm')) = c.month
+   and to_number(to_char(join_date,'yyyy')) = c.yr
+) 
+ select month,max(ad) active_drivers,count(distinct ad.ride_id) accepted_rides
+  from ( select yr,month,count(driver_id) over(order by yr,month) ad
+ from cte2 ) T
+  left join Rides r on T.yr = to_number(to_char(requested_at,'yyyy'))
+      and T.month = to_number(to_char(requested_at,'mm'))
+  left outer join AcceptedRides ad on ad.ride_id = r.ride_id
+ where yr = 2020
+ group by yr,month
+order by yr,month
+
+
+--solution2 with edge cases missing 
+/* Write your T-SQL query statement below */
+with cte as (
+    select min(join_date) join_date,max(join_date) join_date_max
+     from Drivers
+     union all
+    select dateadd(month,1,join_date),join_date_max from cte
+    where join_date < join_date_max
+) ,
+cte2 as ( select year(c.join_date) yr ,month(c.join_date) mn,driver_id
+   from cte c
+    left join drivers d 
+     on year(c.join_date) = year(d.join_date)
+      and month(c.join_date) = month(d.join_date)
+) 
+ select mn month,max(ad) active_drivers,count(ad.ride_id) accepted_rides
+  from ( select yr,mn,count(driver_id) over( order by yr,mn) ad
+  from cte2 ) T
+  left join Rides r on T.yr = year(requested_at) and T.mn = month(requested_at)
+  left outer join AcceptedRides ad on ad.ride_id = r.ride_id
+ where T.yr = 2020
+ group by yr,mn
+  order by mn
+
+68. https://leetcode.com/problems/hopper-company-queries-ii/ 
+
+Hopper Company Queries II
+
+Share
+SQL Schema
+Table: Drivers
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| driver_id   | int     |
+| join_date   | date    |
++-------------+---------+
+driver_id is the primary key for this table.
+Each row of this table contains the drivers ID and the date they joined the Hopper company.
+ 
+
+Table: Rides
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| ride_id      | int     |
+| user_id      | int     |
+| requested_at | date    |
++--------------+---------+
+ride_id is the primary key for this table.
+Each row of this table contains the ID of a ride, the users ID that requested it, and the day they requested it.
+There may be some ride requests in this table that were not accepted.
+ 
+
+Table: AcceptedRides
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| ride_id       | int     |
+| driver_id     | int     |
+| ride_distance | int     |
+| ride_duration | int     |
++---------------+---------+
+ride_id is the primary key for this table.
+Each row of this table contains some information about an accepted ride.
+It is guaranteed that each accepted ride exists in the Rides table.
+ 
+
+Write an SQL query to report the percentage of working drivers (working_percentage) for each month of 2020 where:
+
+
+Note that if the number of available drivers during a month is zero, we consider the working_percentage to be 0.
+
+Return the result table ordered by month in ascending order, where month is the months number (January is 1, February is 2, etc.). Round working_percentage to the nearest 2 decimal places.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Drivers table:
++-----------+------------+
+| driver_id | join_date  |
++-----------+------------+
+| 10        | 2019-12-10 |
+| 8         | 2020-1-13  |
+| 5         | 2020-2-16  |
+| 7         | 2020-3-8   |
+| 4         | 2020-5-17  |
+| 1         | 2020-10-24 |
+| 6         | 2021-1-5   |
++-----------+------------+
+Rides table:
++---------+---------+--------------+
+| ride_id | user_id | requested_at |
++---------+---------+--------------+
+| 6       | 75      | 2019-12-9    |
+| 1       | 54      | 2020-2-9     |
+| 10      | 63      | 2020-3-4     |
+| 19      | 39      | 2020-4-6     |
+| 3       | 41      | 2020-6-3     |
+| 13      | 52      | 2020-6-22    |
+| 7       | 69      | 2020-7-16    |
+| 17      | 70      | 2020-8-25    |
+| 20      | 81      | 2020-11-2    |
+| 5       | 57      | 2020-11-9    |
+| 2       | 42      | 2020-12-9    |
+| 11      | 68      | 2021-1-11    |
+| 15      | 32      | 2021-1-17    |
+| 12      | 11      | 2021-1-19    |
+| 14      | 18      | 2021-1-27    |
++---------+---------+--------------+
+AcceptedRides table:
++---------+-----------+---------------+---------------+
+| ride_id | driver_id | ride_distance | ride_duration |
++---------+-----------+---------------+---------------+
+| 10      | 10        | 63            | 38            |
+| 13      | 10        | 73            | 96            |
+| 7       | 8         | 100           | 28            |
+| 17      | 7         | 119           | 68            |
+| 20      | 1         | 121           | 92            |
+| 5       | 7         | 42            | 101           |
+| 2       | 4         | 6             | 38            |
+| 11      | 8         | 37            | 43            |
+| 15      | 8         | 108           | 82            |
+| 12      | 8         | 38            | 34            |
+| 14      | 1         | 90            | 74            |
++---------+-----------+---------------+---------------+
+Output: 
++-------+--------------------+
+| month | working_percentage |
++-------+--------------------+
+| 1     | 0.00               |
+| 2     | 0.00               |
+| 3     | 25.00              |
+| 4     | 0.00               |
+| 5     | 0.00               |
+| 6     | 20.00              |
+| 7     | 20.00              |
+| 8     | 20.00              |
+| 9     | 0.00               |
+| 10    | 0.00               |
+| 11    | 33.33              |
+| 12    | 16.67              |
++-------+--------------------+
+Explanation: 
+By the end of January --> two active drivers (10, 8) and no accepted rides. The percentage is 0%.
+By the end of February --> three active drivers (10, 8, 5) and no accepted rides. The percentage is 0%.
+By the end of March --> four active drivers (10, 8, 5, 7) and one accepted ride by driver (10). The percentage is (1 / 4) * 100 = 25%.
+By the end of April --> four active drivers (10, 8, 5, 7) and no accepted rides. The percentage is 0%.
+By the end of May --> five active drivers (10, 8, 5, 7, 4) and no accepted rides. The percentage is 0%.
+By the end of June --> five active drivers (10, 8, 5, 7, 4) and one accepted ride by driver (10). The percentage is (1 / 5) * 100 = 20%.
+By the end of July --> five active drivers (10, 8, 5, 7, 4) and one accepted ride by driver (8). The percentage is (1 / 5) * 100 = 20%.
+By the end of August --> five active drivers (10, 8, 5, 7, 4) and one accepted ride by driver (7). The percentage is (1 / 5) * 100 = 20%.
+By the end of September --> five active drivers (10, 8, 5, 7, 4) and no accepted rides. The percentage is 0%.
+By the end of October --> six active drivers (10, 8, 5, 7, 4, 1) and no accepted rides. The percentage is 0%.
+By the end of November --> six active drivers (10, 8, 5, 7, 4, 1) and two accepted rides by two different drivers (1, 7). The percentage is (2 / 6) * 100 = 33.33%.
+By the end of December --> six active drivers (10, 8, 5, 7, 4, 1) and one accepted ride by driver (4). The percentage is (1 / 6) * 100 = 16.67%.
+
+--solution
+/* Write your PL/SQL query statement below */
+
+with cte(yr,month) as (
+    select 2020,1 from dual 
+     union all
+    select 2020,month + 1 from cte 
+    where month+1 <= 12
+) ,
+cte2 as (
+select 
+ coalesce(yr,to_number(to_char(join_date,'yyyy'))) yr,
+ coalesce(month,to_number(to_char(join_date,'mm'))) month,
+ driver_id
+from cte c 
+ full outer join Drivers d 
+  on to_number(to_char(join_date,'mm')) = c.month
+   and to_number(to_char(join_date,'yyyy')) = c.yr
+) 
+ select month,
+  case when max(ad) = 0 then 0 
+       else round((count(distinct ad.driver_id)/max(ad) * 100),2) 
+   end
+   working_percentage
+  from ( select yr,month,count(driver_id) over(order by yr,month) ad
+ from cte2 ) T
+  left join Rides r on T.yr = to_number(to_char(requested_at,'yyyy'))
+      and T.month = to_number(to_char(requested_at,'mm'))
+  left outer join AcceptedRides ad on ad.ride_id = r.ride_id
+ where yr = 2020
+ group by yr,month
+ order by yr,month
+
