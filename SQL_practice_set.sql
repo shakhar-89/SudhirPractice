fb4db36e8344588ad9600c72de415fb27fd09a8f
sudhir Kumar <shakhar.89@gmail.com>
2022-05-19 21:51:05 +0530

    Update SQL_practice_set.sql

--- SQL_practice_set.sql
+++ SQL_practice_set.sql
@@ -4855,25 +4855,25 @@ We can hire all three juniors with the remaining budget.
 
 --solution
 /* Write your PL/SQL query statement below */
-with S as (
+with S as ( -- adding maximum number of seniors till 70000
 select experience,count(employee_id) ac ,max(total_sal) maxSal
 from ( 
     select 
         experience,employee_id,sum(salary) over(order by salary,employee_id) total_sal
     from Candidates where experience = 'Senior'
     union all
-    select 'Senior',NULL,0 from dual
+    select 'Senior',NULL,0 from dual -- in case 0 senior
 ) where total_sal <=70000
 group by experience
 ),
-J as (
+J as ( -- adding maximum number of juniors till 70000 - sum(of seniors)
 select T.experience,count(T.employee_id) accepted_candidates
 from ( 
     select 
         experience,employee_id,sum(salary) over(order by salary,employee_id) total_sal
     from Candidates where experience = 'Junior'
         union all
-    select 'Junior',NULL,0 from dual
+    select 'Junior',NULL,0 from dual -- in case 0 junior
 ) T full outer join S on 1=1  
 where T.total_sal <= 70000 - S.maxSal
 group by T.experience )
@@ -4881,7 +4881,108 @@ select experience,ac accepted_candidates from S
 union all
 select experience,accepted_candidates from J
 
+74. https://leetcode.com/problems/the-number-of-seniors-and-juniors-to-join-the-company-ii/ 
 
+The Number of Seniors and Juniors to Join the Company II
 
+Table: Candidates
 
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| employee_id | int  |
+| experience  | enum |
+| salary      | int  |
++-------------+------+
+employee_id is the primary key column for this table.
+experience is an enum with one of the values ('Senior', 'Junior').
+Each row of this table indicates the id of a candidate, their monthly salary, and their experience.
+The salary of each candidate is guaranteed to be unique.
+ 
 
+A company wants to hire new employees. The budget of the company for the salaries is $70000. The companys criteria for hiring are:
+
+Keep hiring the senior with the smallest salary until you cannot hire any more seniors.
+Use the remaining budget to hire the junior with the smallest salary.
+Keep hiring the junior with the smallest salary until you cannot hire any more juniors.
+Write an SQL query to find the ids of seniors and juniors hired under the mentioned criteria.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input:
+Candidates table:
++-------------+------------+--------+
+| employee_id | experience | salary |
++-------------+------------+--------+
+| 1           | Junior     | 10000  |
+| 9           | Junior     | 15000  |
+| 2           | Senior     | 20000  |
+| 11          | Senior     | 16000  |
+| 13          | Senior     | 50000  |
+| 4           | Junior     | 40000  |
++-------------+------------+--------+
+Output: 
++-------------+
+| employee_id |
++-------------+
+| 11          |
+| 2           |
+| 1           |
+| 9           |
++-------------+
+Explanation: 
+We can hire 2 seniors with IDs (11, 2). Since the budget is $70000 and the sum of their salaries is $36000, we still have $34000 but they are not enough to hire the senior candidate with ID 13.
+We can hire 2 juniors with IDs (1, 9). Since the remaining budget is $34000 and the sum of their salaries is $25000, we still have $9000 but they are not enough to hire the junior candidate with ID 4.
+Example 2:
+
+Input:
+Candidates table:
++-------------+------------+--------+
+| employee_id | experience | salary |
++-------------+------------+--------+
+| 1           | Junior     | 25000  |
+| 9           | Junior     | 10000  |
+| 2           | Senior     | 85000  |
+| 11          | Senior     | 80000  |
+| 13          | Senior     | 90000  |
+| 4           | Junior     | 30000  |
++-------------+------------+--------+
+Output: 
++-------------+
+| employee_id |
++-------------+
+| 9           |
+| 1           |
+| 4           |
++-------------+
+Explanation: 
+We cannot hire any seniors with the current budget as we need at least $80000 to hire one senior.
+We can hire all three juniors with the remaining budget.
+
+--solution
+with S as ( -- adding maximum number of seniors till 70000
+select employee_id,total_sal
+from ( 
+    select 
+        employee_id,sum(salary) over(order by salary,employee_id) total_sal
+    from Candidates where experience = 'Senior'
+) where total_sal <=70000
+),
+J as ( -- adding maximum number of juniors till 70000 - sum(of seniors)
+select T.employee_id 
+from ( 
+    select 
+        employee_id,sum(salary) over(order by salary) total_sal
+    from Candidates where experience = 'Junior'
+
+) T 
+where T.total_sal <= 70000 - (select coalesce(max(total_sal),0) from S))
+ select employee_id from S
+ union all
+ select employee_id from J
